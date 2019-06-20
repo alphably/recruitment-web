daa575bedd48af9d26dfc6469122f0675eb12aef
bao <bao@bao.pc>
2019-06-20 21:02:11 +0200

    generation getter and setter of Book class

--- src/main/java/fr/d2factory/libraryapp/book/Book.java
+++ src/main/java/fr/d2factory/libraryapp/book/Book.java
@@ -13,4 +13,29 @@ public class Book {
         this.author = author;
         this.isbn = isbn;
     }
+    
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public ISBN getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(ISBN isbn) {
+		this.isbn = isbn;
+	}
+
 }
